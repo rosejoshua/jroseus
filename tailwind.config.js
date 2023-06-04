d3698672290f532ec94d9cadc1d662d4f3db1626
Joshua Rose <jlr007@protonmail.com>
2023-06-04 18:46:31 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custombg': '#2b2b2b',
+      }
+    },
+  },
+  plugins: [],
+}
+
